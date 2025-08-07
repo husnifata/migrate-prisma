fb64e7f3c1bc52207b89ef03d83a9a882a0fb3aa
husnifatah <husnifatah0@gmail.com>
2025-08-07 10:34:10 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Ini belajar dummy saja jadi isinya begini aja ya hehe");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Halo dari Railway!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server jalan di http://localhost:${PORT}`);
+});
